8abd5ca491122999f653c6263b854de664f9d037
Youssef Adly <118239513+Youssef-Adly@users.noreply.github.com>
2024-07-05 02:05:43 +0300

    Guest Guard Toast

--- src/utils/IsGuestGuard.js
+++ src/utils/IsGuestGuard.js
@@ -2,17 +2,23 @@ import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
 import React from "react";
+import { toastError } from "./ToastsFunctions";
 
 const PrivateGuestRoute = ({ children }) => {
   const user = useSelector((state) => state.Auth.user); // Replace with your user selector
 
   // Handle potential missing user state (optional)
   if (user.user_type === "guest") {
+    setTimeout(() => {
+      toastError("Guest, Please Login First");
+    }, 100);
     // Or redirect to login if user state is not yet available
     return <Navigate to="/login" />;
+  } else {
+    return children;
   }
 
-  return user.user_type === "guest" ? <Navigate to="/login" /> : children;
+  // return user.user_type === "guest" ? <Navigate to="/login" /> : children;
 };
 
 export default PrivateGuestRoute;
